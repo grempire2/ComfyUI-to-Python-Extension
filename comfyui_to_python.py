d3c9ae0cf297a80fec77a42b5e1537f57dfe7d33
forevermosa <148589884+grempire2@users.noreply.github.com>
2024-03-16 03:14:42 -0700

    update

--- comfyui_to_python.py
+++ comfyui_to_python.py
@@ -10,6 +10,9 @@ from typing import Dict, List, Any, Callable, Tuple
 
 import black
 
+# Append the script's directory to sys.path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(script_dir)
 
 from utils import import_custom_nodes, find_path, add_comfyui_directory_to_sys_path, add_extra_model_paths, get_value_at_index
 
