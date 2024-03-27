094287d4c779b5d3cfc59ed624199cfdf4f1d442
grempire2 <greatredempire@gmail.com>
2024-03-27 08:26:50 -0700

    update

--- convert_workflow.bat
+++ convert_workflow.bat
@@ -1,4 +1,16 @@
 @echo off
 X:\ComfyUI_windows_portable\python_embeded\python.exe X:\ComfyUI_windows_portable\ComfyUI\ComfyUI-to-Python-Extension\comfyui_to_python.py
-powershell -Command "(Get-Content X:\ComfyUI_windows_portable\ComfyUI\ComfyUI-to-Python-Extension\workflow_api.py) -replace 'for q in range\(10\):', 'for q in range(3):' | Set-Content X:\ComfyUI_windows_portable\ComfyUI\ComfyUI-to-Python-Extension\workflow_api.py"
+
+set "file=X:\ComfyUI_windows_portable\ComfyUI\ComfyUI-to-Python-Extension\workflow_api.py"
+set "tempFile=temp.py"
+
+:: Create a new temporary file
+powershell -Command "$content = Get-Content %file%; $newContent = @(); foreach ($line in $content) { if ($line -match 'negative=') { $newLine = $line -replace 'negative=.*?,', 'negative=negative_,'; $newContent += $newLine } else { $newContent += $line } }; $newContent | Set-Content %tempFile%"
+
+:: Add the new variable at the top of the script after the imports
+powershell -Command "$content = Get-Content %tempFile%; $content[0], 'negative_ = \"\"', $content[1..($content.Length - 1)] | Set-Content %tempFile%"
+
+:: Replace the original file with the modified one
+move /Y %tempFile% %file%
+
 pause
