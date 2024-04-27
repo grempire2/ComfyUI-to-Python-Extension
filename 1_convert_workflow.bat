7b80998a187ada68fb9b0699c7f28d8a9d67b373
forevermosa <148589884+grempire2@users.noreply.github.com>
2024-04-26 22:46:29 -0700

    update

--- 1_convert_workflow.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-X:\ComfyUI_windows_portable\python_embeded\python.exe X:\ComfyUI_windows_portable\ComfyUI\ComfyUI-to-Python-Extension\comfyui_to_python.py
-
-pause
--- 2_test_workflow.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-X:\ComfyUI_windows_portable\python_embeded\python.exe X:\ComfyUI_windows_portable\ComfyUI\ComfyUI-to-Python-Extension\workflow_api.py
-pause
