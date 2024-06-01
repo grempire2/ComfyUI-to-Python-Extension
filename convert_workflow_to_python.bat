7eb019ad7def1da866d3eaf6ee714e0837783a29
forevermosa <148589884+grempire2@users.noreply.github.com>
2024-06-01 09:30:00 -0700

    update

--- convert_workflow_to_python.bat
+++ convert_workflow_to_python.bat
@@ -1,4 +1,7 @@
 @echo off
 X:\ComfyUI_windows_portable\python_embeded\python.exe X:\ComfyUI_windows_portable\ComfyUI\ComfyUI-to-Python-Extension\comfyui_to_python.py
 
+:: Rename the output file
+rename workflow_api.py comfy_gen.py
+
 pause
